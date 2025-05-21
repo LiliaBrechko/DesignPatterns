3ef5342df385ed46bbd5ac941a2bb39d000721e1
Lilia Brechko <lilia_brechko@epam.com>
2025-05-21 21:05:38 +0300

    Add Adapter

---
+++ DesignPatterns/Adapter/AdapterInchesToMeters.cs
@@ -0,0 +1,18 @@
+﻿namespace Adapter
+{
+    public class AdapterInchesToMeters : IMetricConverter
+    {
+        private ImperialConverter _imperialConverter;
+        public AdapterInchesToMeters(ImperialConverter imperialConverter)
+        {
+            _imperialConverter = imperialConverter;
+        }
+        public double GetMeters()
+        {
+            double inches = _imperialConverter.GetInches();
+            double meters = inches * 0.0254;
+            Console.WriteLine($"{inches} inches is {meters} meters");
+            return meters;
+        }
+    }
+}
---
+++ DesignPatterns/Adapter/IMetricConverter.cs
@@ -0,0 +1,7 @@
+﻿namespace Adapter
+{
+    public interface IMetricConverter
+    {
+        double GetMeters();
+    }
+}
