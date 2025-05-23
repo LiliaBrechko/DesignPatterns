088ce4964fac78e51c8ff96138c35ce68f346173
Lilia Brechko <lilia_brechko@epam.com>
2025-05-23 18:54:16 +0300

    Add Builder

---
+++ DesignPatterns/Builder/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Builder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserBuilder userBuilder = new UserBuilder();
+            UserDirector userDirector = new UserDirector(userBuilder);
+
+            User Lilia = userBuilder.WithFirstName("Lilia").WithLastName("Brechko").WithEmail("lilia.brechko@gmail.com").WithAge(32).Build();
+
+            User LiliaAdmin = userDirector.CreateAdmin(Lilia.FirstName, Lilia.LastName, Lilia.Age, Lilia.Email);
+
+            Console.WriteLine(Lilia.ToString());
+        }
+    }
+}
