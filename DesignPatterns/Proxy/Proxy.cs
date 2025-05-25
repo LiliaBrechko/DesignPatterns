3db94e9c161902111b4fa20fa74ae3724ecec12d
Lilia Brechko <lilia_brechko@epam.com>
2025-05-25 11:41:42 +0300

    Add Proxy

---
+++ DesignPatterns/Proxy/Proxy.cs
@@ -0,0 +1,28 @@
+﻿namespace Proxy
+{
+    public class Proxy : ISubject
+    {
+        private readonly SecretDocument _secretDocument;
+        private readonly User _user;
+
+        public Proxy(User user, string secret)
+        {
+            _user = user;
+            _secretDocument = new SecretDocument(secret);
+        }
+        public void Open()
+        {
+            Console.WriteLine($"User {_user.Name} is trying to access the secret document.");
+
+            if (_user.isAdmin)
+            {
+                Console.WriteLine($"User {_user.Name} is an admin. Access granted.");
+                _secretDocument.Open();
+            }
+            else
+            {
+                Console.WriteLine("Access denied. You are not an admin.");
+            }
+        }
+    }
+}
